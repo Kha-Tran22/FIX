ae22c0471ff7c90337c81dc335b0d27b2cc046b7
Kha Tran <tuankhavntv@gmail.com>
2024-09-09 20:45:19 +0700

    header exercise1

--- SourceCode/Core/Inc/exercise1.h
+++ SourceCode/Core/Inc/exercise1.h
@@ -8,6 +8,13 @@
 #ifndef INC_EXERCISE1_H_
 #define INC_EXERCISE1_H_
 
+#include"main.h"
+
+#define LED_ON 1
+#define LED_OFF 0
+
+void init_exercise1();
+void run_exercise1();
 
 
 #endif /* INC_EXERCISE1_H_ */
